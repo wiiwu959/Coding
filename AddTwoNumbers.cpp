57d262a7c4afd02765071328f002438005daeca9
wiiwu959 <43946620+wiiwu959@users.noreply.github.com>
2019-04-18 02:02:16 +0800

    Problem set number 2, Add Two Number

---
+++ AddTwoNumbers.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+    ListNode(int x) : val(x), next(NULL) {}
+ };
+
+class Solution {
+public:
+	ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+		ListNode* ansHead = new ListNode(0);
+		ListNode* ans = ansHead;
+		int carry = 0;
+		while (l1 != NULL || l2 != NULL) {
+			int a = 0, b = 0;
+			if (l1 != NULL) { a = l1->val; }
+			if (l2 != NULL) { b = l2->val; }
+			ans->next = new ListNode((a + b + carry) % 10);
+			carry = (a + b + carry) / 10;
+			if (l1 != NULL) { l1 = l1->next; }
+			if (l2 != NULL) { l2 = l2->next; }
+			ans = ans->next;
+		}
+
+		if (carry != 0)
+		{
+			ans->next = new ListNode(carry);
+		}
+		return ansHead->next;
+	}
+};
