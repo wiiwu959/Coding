96fa1f03042a24bc280697664eb3834a56a9b23f
Wiiwu <iriswu959@gmail.com>
2019-04-20 00:08:38 +0800

    better way doing 003.

--- 003-LongestSubstring.cpp
+++ 003-LongestSubstring.cpp
@@ -5,7 +5,44 @@
 using namespace std;
 
 // 網路上說叫做sliding windows的方法
+// 這樣的速度會有O(n)
 class Solution {
+public:
+	int lengthOfLongestSubstring(string s) {
+		bool all[256];
+		for (int i = 0; i < 256; i++)
+		{
+			all[i] = false;
+		}
+		int max = 0, counter = 0, i = 0, j = 0;
+		while (i < s.size() && j < s.size())
+		{
+
+			if (all[s[j]] != false)				// 找到有重複，就從前面開始往後刪掉，直到沒有重複為止
+			{
+				counter--;
+				all[s[i]] = false;
+				i++;
+			}
+			else								// 若是沒有重複，就繼續往下做
+			{
+				counter++;
+				all[s[j]] = true;
+				j++;
+			}
+			max = counter > max ? counter : max;
+
+		}
+		return max;
+	}
+};
+
+
+// 如果和第一個做法一樣，但是用map的話
+// 因為find是利用紅黑樹演算法，所以他的速度是log n
+// 所以比較慢!!
+// https://stackoverflow.com/questions/9961742/time-complexity-of-find-in-stdmap
+class Solution2 {
 public:
 	int lengthOfLongestSubstring(string s) {
 		map<char, int> all;
@@ -13,13 +50,13 @@ public:
 		while (i < s.size() && j < s.size())
 		{
 			
-			if (all.find(s[j]) != all.end())	// 找到有重複，就從前面開始往後刪掉，直到沒有重複為止
+			if (all.find(s[j]) != all.end())
 			{
 				counter --;
 				all.erase(s[i]);
 				i++;
 			}
-			else								// 若是沒有重複，就繼續往下做
+			else
 			{
 				counter++;
 				all[s[j]] = j;
@@ -33,7 +70,7 @@ public:
 };
 
 // 暴力法，超~~~級慢到不行絕對不要用糟糕爆了
-class Solution2 {
+class Solution3 {
 	int lengthOfLongestSubstring(string s) {
 		map<char, int> all;
 		map<char, int>::iterator iter;
